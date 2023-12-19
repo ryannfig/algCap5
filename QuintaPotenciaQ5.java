59e8ae85922055a577888fcb5d3ced094788f839
ryannfig <113732663+ryannfig@users.noreply.github.com>
2023-12-19 12:24:11 -0300

    Update QuintaPotenciaQ5.java

--- QuintaPotenciaQ5.java
+++ QuintaPotenciaQ5.java
@@ -1,1 +1,18 @@
+package quintapotencia;
+import javax.swing.JOptionPane;
+public class QuintaPotencia {
 
+    public static void main(String[] args) {
+      
+        // Solicita o número usando JOptionPane
+        String numeroString = JOptionPane.showInputDialog("Digite um número:");
+        double numero = Double.parseDouble(numeroString);
+
+        // Calcula o número elevado à quinta potência
+        double resultado = Math.pow(numero, 5);
+
+        // Exibe o resultado usando JOptionPane
+        JOptionPane.showMessageDialog(null, "O número elevado à quinta potência é: " + resultado);
+        
+    }
+}
